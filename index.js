8564ac499ca70fe752c15c36f5b8693a9beeacd0
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-09-08 02:41:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import "dotenv/config";
+// https://docs.github.com/en/rest/actions/workflow-runs?apiVersion=2022-11-28#list-workflow-runs-for-a-repository
+let runs = await fetch(
+  `https://api.github.com/repos/${process.env.OWNER}/${process.env.REPO}/actions/runs?per_page=100`,
+  {
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      "X-Github-Api-Version": "2022-11-28",
+    },
+  },
+).then((res) => res.json());
+console.log(`Total Count: ${runs.total_count}`);
+for (let run of runs.workflow_runs) {
+  // https://docs.github.com/en/rest/actions/workflow-runs?apiVersion=2022-11-28#delete-a-workflow-run
+  let delResult = await fetch(
+    `https://api.github.com/repos/${process.env.OWNER}/${process.env.REPO}/actions/runs/${run.id}`,
+    {
+      method: "DELETE",
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        "X-Github-Api-Version": "2022-11-28",
+      },
+    },
+  );
+  if (delResult.status == 204) {
+    console.log(`204 - ${run.name} (${run.id})`);
+  } else {
+    console.log(
+      `${delResult.status} - ${run.name} (${run.id}) - ${await delResult.text()}`,
+    );
+  }
+}
