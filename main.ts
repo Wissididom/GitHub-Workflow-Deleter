268756e1b193aeb8d5e2a7521b9b475d330b6c55
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-10-23 09:16:11 +0200

    Use Deno (#1)

--- main.ts
+++ main.ts
@@ -1,26 +1,31 @@
-import "dotenv/config";
+import type { WorkflowRuns } from "./workflow-runs.ts";
+
 // https://docs.github.com/en/rest/actions/workflow-runs?apiVersion=2022-11-28#list-workflow-runs-for-a-repository
-let runs = await fetch(
-  `https://api.github.com/repos/${process.env.OWNER}/${process.env.REPO}/actions/runs`,
+const runs: WorkflowRuns = await fetch(
+  `https://api.github.com/repos/${Deno.env.get("OWNER")}/${
+    Deno.env.get("REPO")
+  }/actions/runs`,
   {
     method: "GET",
     headers: {
       Accept: "application/vnd.github+json",
-      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      Authorization: `Bearer ${Deno.env.get("GITHUB_TOKEN")}`,
       "X-Github-Api-Version": "2022-11-28",
     },
   },
-).then((res) => res.json());
+).then((res) => res.json()).then((res) => res as WorkflowRuns);
 console.log(`Total Count: ${runs.total_count}`);
-for (let run of runs.workflow_runs) {
+for (const run of runs.workflow_runs) {
   // https://docs.github.com/en/rest/actions/workflow-runs?apiVersion=2022-11-28#delete-a-workflow-run
-  let delResult = await fetch(
-    `https://api.github.com/repos/${process.env.OWNER}/${process.env.REPO}/actions/runs/${run.id}`,
+  const delResult: Response = await fetch(
+    `https://api.github.com/repos/${Deno.env.get("OWNER")}/${
+      Deno.env.get("REPO")
+    }/actions/runs/${run.id}`,
     {
       method: "DELETE",
       headers: {
         Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        Authorization: `Bearer ${Deno.env.get("GITHUB_TOKEN")}`,
         "X-Github-Api-Version": "2022-11-28",
       },
     },
@@ -29,7 +34,8 @@ for (let run of runs.workflow_runs) {
     console.log(`204 - ${run.name} (${run.id})`);
   } else {
     console.log(
-      `${delResult.status} - ${run.name} (${run.id}) - ${await delResult.text()}`,
+      `${delResult.status} - ${run.name} (${run.id}) - ${await delResult
+        .text()}`,
     );
   }
 }
